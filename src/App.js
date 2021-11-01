5603abd8856479cc6deea5f6e7073e5388adf738
Fernando Fernández Valero <ferval19@gmail.com>
2021-11-01 12:07:39 +0100

    -texto

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hecho en React
         </a>
       </header>
     </div>
