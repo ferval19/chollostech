d85d4305d6d2091fc1df598e850d6d42e069d33b
Fernando Fernández Valero <ferval19@gmail.com>
2021-11-01 12:04:08 +0100

    -ajuste

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const tailwind = require("tailwindcss");
 module.exports = {
     plugins: {
-        tailwindcss("./tailwind.js)
-        require("autoprefixer)
+        tailwindcss("./tailwind.js")
+        require("autoprefixer")
     }
 }
