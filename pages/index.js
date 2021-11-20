e2ca026ec8a969f90fedc10c28fa34c2a32e60b8
Fernando Fernández Valero <ferval19@gmail.com>
2021-11-20 21:48:47 +0100

    -Incluimos tailwindcss -Hero inicial

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,234 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
+import "tailwindcss/tailwind.css";
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Chollos.tech</title>
         <meta name="description" content="Tu sitio de chollos en tecnología." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Bienvenido a Chollos.tech</h1>
+      <main>
+        <div className="relative bg-white overflow-hidden">
+          <div class="max-w-7xl mx-auto">
+            <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+              <svg
+                class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                fill="currentColor"
+                viewBox="0 0 100 100"
+                preserveAspectRatio="none"
+                aria-hidden="true"
+              >
+                <polygon points="50,0 100,0 50,100 0,100" />
+              </svg>
 
-        <p className={styles.description}>
-          Tu web para estar al día de los chollos en tecnología y estilo de vida
-          tecnológico.
-        </p>
+              <div>
+                <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+                  <nav
+                    class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                    aria-label="Global"
+                  >
+                    <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                      <div class="flex items-center justify-between w-full md:w-auto">
+                        <a href="#">
+                          <span class="sr-only">Workflow</span>
+                          <img
+                            class="h-8 w-auto sm:h-10"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                          />
+                        </a>
+                        <div class="-mr-2 flex items-center md:hidden">
+                          <button
+                            type="button"
+                            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            aria-expanded="false"
+                          >
+                            <span class="sr-only">Open main menu</span>
+                            <svg
+                              class="h-6 w-6"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                      <a
+                        href="#"
+                        class="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Destacados
+                      </a>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Ofertas BlackFriday &rarr;</h2>
-            <p>Los chollos mas interesantes del Black Friday.</p>
-          </a>
+                      <a
+                        href="#"
+                        class="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        LifeStyle
+                      </a>
 
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Ordenadores &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                      <a
+                        href="#"
+                        class="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Videojuegos
+                      </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Periféricos &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                      <a
+                        href="#"
+                        class="font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        Perifericos
+                      </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Productividad &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                      <a
+                        href="#"
+                        class="font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        Registrarse
+                      </a>
+                    </div>
+                  </nav>
+                </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Consolas &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                  <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div class="px-5 pt-4 flex items-center justify-between">
+                      <div>
+                        <img
+                          class="h-8 w-auto"
+                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div class="-mr-2">
+                        <button
+                          type="button"
+                          class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                        >
+                          <span class="sr-only">Close main menu</span>
+                          <svg
+                            class="h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="px-2 pt-2 pb-3 space-y-1">
+                      <a
+                        href="#"
+                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        Destacados
+                      </a>
+
+                      <a
+                        href="#"
+                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        LifeStyle
+                      </a>
+
+                      <a
+                        href="#"
+                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        Videojuegos
+                      </a>
+
+                      <a
+                        href="#"
+                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        Perifericos
+                      </a>
+                    </div>
+                    <a
+                      href="#"
+                      class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    >
+                      Registrate
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div class="sm:text-center lg:text-left">
+                  <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <span class="block xl:inline">Bienvenidos a </span>
+                    <span class="block text-indigo-600 xl:inline">
+                      Chollos.tech
+                    </span>
+                  </h1>
+                  <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    Tu web para estar al día de los chollos en tecnología y
+                    estilo de vida tecnológico.
+                  </p>
+                  <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                    <div class="rounded-md shadow">
+                      <a
+                        href="#"
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                      >
+                        Ofertas
+                      </a>
+                    </div>
+                    <div class="mt-3 sm:mt-0 sm:ml-3">
+                      <a
+                        href="#"
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                      >
+                        Black Friday
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </div>
+          <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <img
+              class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+              src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+              alt=""
+            />
+          </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Hecho con{" "}
-          <span className={styles.logo}>
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
