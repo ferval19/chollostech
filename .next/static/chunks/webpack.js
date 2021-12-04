67b0e1d8ac780e98251093fa5721048a6bc6d182
Fernando Fernández Valero <ferval19@gmail.com>
2021-12-04 17:49:32 +0100

    -icono amazon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efbd7599af92276a"; }
+/******/ 		__webpack_require__.h = function() { return "95700e9e264e3e05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/botonAmazon.js
+++ components/botonAmazon.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function BotonAmazon({ enlace }) {
   return (
     <div className="rounded-md shadow mb-4">
@@ -5,7 +7,8 @@ export default function BotonAmazon({ enlace }) {
         href={enlace}
         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-yellow-400 hover:bg-yellow-300 md:py-4 md:text-lg md:px-10"
       >
-        Comprar en Amazon
+        Ver en
+        <Image src="/amazon.svg" alt="Vercel Logo" width={72} height={16} />
       </a>
     </div>
   );
