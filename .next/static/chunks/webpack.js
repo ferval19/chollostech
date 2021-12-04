662642e45d149b16ebb48a6b445568de65c94c25
Fernando Fernández Valero <ferval19@gmail.com>
2021-12-04 12:15:43 +0100

    -Fix nombre producto

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f931c842999d7201"; }
+/******/ 		__webpack_require__.h = function() { return "1ad4910e438d2e2c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/product-title.js
+++ components/product-title.js
@@ -1,7 +1,7 @@
-export default function ProductTitle({ children }) {
+export default function ProductTitle({ title }) {
   return (
     <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-4 text-center md:text-left">
-      {children}
+      {title}
     </h1>
   );
 }
